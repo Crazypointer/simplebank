5a9c9e753a488276857fad6dcbecb6d4b52bf365
ysm119 <1196720398@qq.com>
2024-02-28 15:31:05 +0800

    try

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ set -e
 
 source /app/app.env
 echo "查看环境变量中是否有DB_SOURCE"
+cat /app/app.env
 echo $DB_SOURCE
 echo "run db migration"
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
